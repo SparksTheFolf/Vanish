1126ec8afbaf04b739647c9ca10cba1101dc51c7
Nolan Trapp <officialstackemup@gmail.com>
2020-03-21 18:25:58 -0700

    working Vanish

--- src/com/Vanish/Vplugin/Main.java
+++ src/com/Vanish/Vplugin/Main.java
@@ -1,5 +1,12 @@
 package com.Vanish.Vplugin;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -8,10 +15,38 @@ public class Main extends JavaPlugin{
 	public void onEnable() {
 		System.out.println("Vanish ENABLED");
 	}
+	
+	List<Player> vanished = new ArrayList<>();
 
 	
 	@Override
 	public void onDisable() {
 		System.out.println("Vanish DISABLED");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(sender instanceof Player) {
+			Player player = (Player) sender;
+			
+			if (cmd.getName().equalsIgnoreCase("vanish")) {
+				if(vanished.contains(player)) {
+				   vanished.remove(player);
+				   for(Player target : Bukkit.getOnlinePlayers()) {
+						target.showPlayer(player);
+					}
+				   
+				   player.sendMessage("Un-Vanished");
+				}else {					
+					vanished.add(player);	
+					for(Player target : Bukkit.getOnlinePlayers()) {
+						target.hidePlayer(player);
+					}
+					   player.sendMessage("Vanished");
+				}
+			}
+		}
+		
+		return false;
 	}
 }
